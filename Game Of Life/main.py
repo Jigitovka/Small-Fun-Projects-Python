02491b33ba3b03dff428a8c2d87cb0337b6a8093
jigitovka <vardapetyandavit3@gmail.com>
2025-11-30 20:09:34 +0400

    Separate functionality

---
+++ Game Of Life/main.py
@@ -0,0 +1,21 @@
+from board import Board
+
+# Simple rendering function
+def render(board_param) -> None:
+    to_render = board_param
+    for row in to_render:
+        row_str = ''
+        for elem in range(len(row)):
+            if row[elem] == 0:
+                row_str += '□'
+            else:
+                row_str += '■'
+        print(row_str)
+
+if __name__ == "__main__":
+    brd = Board(3, 3)
+    render(brd.assign_random_state())
+    print("Delimiter here!")
+    render(brd.next_board_state())
+    print("Delimiter here!")
+    render(brd.assign_random_state())
